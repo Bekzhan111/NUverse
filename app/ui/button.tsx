45914d3053f117ca60890eba17087c27bf80f419
batizhamal <batizhamal@gmail.com>
2024-11-18 15:07:31 +0500

    nuverse login

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -9,7 +9,7 @@ export function Button({ children, className, ...rest }: ButtonProps) {
     <button
       {...rest}
       className={clsx(
-        'flex h-10 items-center rounded-lg bg-[#457b9d] px-4 text-sm font-medium text-white transition-colors hover:bg-opacity-90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#457b9d] active:bg-opacity-80 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
+        'flex h-10 items-center rounded-lg bg-[#c6862c] px-4 text-sm font-medium text-white transition-colors hover:bg-opacity-90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#457b9d] active:bg-opacity-80 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
         className,
       )}
     >

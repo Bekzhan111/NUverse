fb221ffffd10490f14f12ca9172e5e8d0eb26da9
Bekzhan <abdullayev.beka.2003@gmail.com>
2025-04-24 15:44:12 +0500

    Changrs to deploy

--- app/dashboard/news/page.tsx
+++ app/dashboard/news/page.tsx
@@ -1,23 +1,23 @@
-import Pagination from '@/app/ui/invoices/pagination';
-import Search from '@/app/ui/search';
-import { lusitana } from '@/app/ui/fonts';
-import { NewsTableSkeleton } from '@/app/ui/skeletons';
-import { Suspense } from 'react';
-import NewsTable from '@/app/ui/news/table';
-import { fetchNewsPages } from '@/app/lib/data';
-import { Button } from '@/app/ui/button';
-import Link from 'next/link';
-import { PlusIcon } from '@heroicons/react/24/outline';
+import Pagination from "@/app/ui/invoices/pagination";
+import Search from "@/app/ui/search";
+import { lusitana } from "@/app/ui/fonts";
+import { NewsTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+import NewsTable from "@/app/ui/news/table";
+import { fetchNewsPages } from "@/app/lib/data";
+import { Button } from "@/app/ui/button";
+import Link from "next/link";
+import { PlusIcon } from "@heroicons/react/24/outline";
 
-export default async function Page({
-  searchParams,
-}: {
+interface PageProps {
   searchParams?: {
     query?: string;
     page?: string;
   };
-}) {
-  const query = searchParams?.query || '';
+}
+
+export default async function Page({ searchParams }: PageProps) {
+  const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
   const totalPages = await fetchNewsPages(query);
 
@@ -29,11 +29,11 @@ export default async function Page({
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search news..." />
         <Link
-            href="/dashboard/news/create"
-            className="flex h-10 items-center rounded-lg bg-[#457b9d] px-4 text-sm font-medium text-white transition-colors hover:bg-[#396b90] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+          href="/dashboard/news/create"
+          className="flex h-10 items-center rounded-lg bg-[#457b9d] px-4 text-sm font-medium text-white transition-colors hover:bg-[#396b90] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
         >
-            <span className="hidden md:block">Publish news</span>{' '}
-            <PlusIcon className="h-5 md:ml-4" />
+          <span className="hidden md:block">Publish news</span>{" "}
+          <PlusIcon className="h-5 md:ml-4" />
         </Link>
       </div>
       <Suspense key={query + currentPage} fallback={<NewsTableSkeleton />}>
